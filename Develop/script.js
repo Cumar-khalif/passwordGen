925b0dd54bed17c853a93357ea31f6d26a56fb0d
omar khalif <73026620+Cumar-khalif@users.noreply.github.com>
2021-07-07 22:14:48 -0500

    worked on readme file

--- Develop/script.js
+++ Develop/script.js
@@ -11,4 +11,41 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
+function generatePassword() {
+  
+  var myPassword = ''
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '~', ':']
+var UpperCases = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCases = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var myLength = prompt('How many characters do you want for your password?');
+
+while(myLength === '' || !parseInt(myLength) || myLength < 8 || myLength > 128) {
+  myLength = prompt('Error! Please MAKE SURE PASSWORD LENGTH IS BETWEEN 8 AND 128 CHARACTERS');
+}
+ 
+ var containsLowerCases = confirm('Do you want lowercase characters?');
+
+ 
+ var containsUpperCases = confirm('Do you want uppercase characters?');
+
+ 
+ var containsNumbers = confirm('Do you want numbers?');
+
+ var containsSpecialCharacters = confirm('Do you want special characters?');
+
+ var finalPool = [];
+
+
+
+
+
+
+
+
+
+
+
+};
---
+++ readmee.md
@@ -0,0 +1,10 @@
+This website i have created have some awesome features. 
+you are presented with a page that reads password generator.
+to generate a new password you are supoosed to click on the generate button where you will be presented with a series of alert question.
+to select it you hit ok or to not select it you click cancel. 
+after the series of alerts are over, your final pasword is ginerated in the small box presented infront of you containing the length and characters you have choosen.
+
+
+## screenshot 
+
+![](../images/)
